187c0d6c9b32416c786449f0ed1e9cc64be0f08b
DEICIDIA <43384189+DEICIDIA@users.noreply.github.com>
2020-11-06 23:12:12 +0100

    img

--- app.py
+++ app.py
@@ -7,17 +7,18 @@ import train
 import chess.pgn
 import webbrowser
 import app
+from flask import Markup
 
 webbrowser.open('http://127.0.0.1:5000/', new=2)
 pgn = open("data/db_1.pgn")
 first_game = chess.pgn.read_game(pgn)
 board = first_game.board()
-img = chess.svg.board(board, size=600)
+img = chess.svg.board(board, size=800)
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', user_image = img)
+    return render_template('index.html', img=Markup(img))
 
 app.run()
