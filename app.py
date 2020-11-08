829d92e8ba87d461c5d180659cf510539a925f2e
Trithuan <36200155+Trithuan@users.noreply.github.com>
2020-11-08 17:15:46 +0100

    Revert "n"
    This reverts commit 725c2d72e5fbac53c1cca32f82563abe1df52f60.

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import chess.pgn
 import webbrowser
 from flask import Flask, Response, request, render_template, jsonify
 
-webbrowser.open('http://127.0.0.1:5000/', new=2)
+#webbrowser.open('http://127.0.0.1:5000/', new=2)
 
 app = Flask(__name__)
 
@@ -15,19 +15,13 @@ board = chess.Board()
 
 @app.route('/')
 def index():
-	return render_template('index.html')
-	
-@app.route('/move', methods = ['POST'])
+    return render_template('index.html')
+    
+@app.route('/move')
 def move():
-	jsdata = request.form
-	print('\n')
-	# print(jsdata, file=sys.stderr)
-	# print('\n')
-	res = input(request.form['source'] + request.form['target'])
-	move = chess.Move.from_uci(res)
-	board.push(move)
-	reponse = app.response_class(response = board.fen(), status=200)
-	return reponse
+    board.push("g1h3")
+    reponse = app.response_class(response = board.fen(), status=200)
+    return reponse
 
 
 app.run(debug=True)
