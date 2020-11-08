fd5521f8209a9cfbd38bb973646509615bc913b5
DEICIDIA <43384189+DEICIDIA@users.noreply.github.com>
2020-11-08 11:52:20 +0100

    update

--- app.py
+++ app.py
@@ -5,26 +5,23 @@ import chess.svg
 import train
 import chess.pgn
 import webbrowser
-from flask import Flask, Response, request, render_template
+from flask import Flask, Response, request, render_template, jsonify
 
 #webbrowser.open('http://127.0.0.1:5000/', new=2)
-pgn = open("data/db_1.pgn")
-first_game = chess.pgn.read_game(pgn)
-board = first_game.board()
-img = chess.svg.board(board, size=800)
 
 app = Flask(__name__)
+
+board = chess.Board()
 
 @app.route('/')
 def index():
     return render_template('index.html')
     
-
-
-
-
-
-
+@app.route('/move')
+def move():
+    board.push("g1h3")
+    reponse = app.response_class(response = board.fen(), status=200)
+    return reponse
 
 
 app.run(debug=True)
--- train.py
+++ train.py
@@ -1,6 +1,6 @@
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
+#import torch
+#import torch.nn as nn
+#import torch.nn.functional as F
 
 
 def evaluate(State) -> float:
