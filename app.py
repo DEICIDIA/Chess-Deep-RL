ad41538b43fc4dd6d14765b66cccbc10614dc7cd
Trithuan <36200155+Trithuan@users.noreply.github.com>
2020-11-09 14:02:29 +0100

    Revert "script pion"
    This reverts commit a2b473aa8cbc1de7033f7f303451896bb409abaf.

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from flask import Flask, Response, request, render_template, jsonify
 webbrowser.open('http://127.0.0.1:5000/', new=2)
 
 app = Flask(__name__)
-print("wtf")
+
 board = chess.Board()
 
 @app.route('/')
@@ -20,7 +20,7 @@ def index():
 @app.route('/move', methods = ['POST'])
 def move():
 	jsdata = request.form
-	print(jsdata, file=sys.stderr)
+	# print(jsdata, file=sys.stderr)
 	print('\n')
 	res = input(request.form['source'] + request.form['target'])
 	move = chess.Move.from_uci(res)
