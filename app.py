f550c97f795eed58552642ad551e88ab58c49521
nathan <thoum.nathan@gmail.com>
2020-11-09 13:53:10 +0100

    j'ai compris

--- app.py
+++ app.py
@@ -13,15 +13,25 @@ app = Flask(__name__)
 
 board = chess.Board()
 
+def computer_move():
+	return random.choice(board.legal_moves)
+
 @app.route('/')
 def index():
     return render_template('index.html')
-    
+
 @app.route('/move')
 def move():
-    board.push("g1h3")
-    reponse = app.response_class(response = board.fen(), status=200)
-    return reponse
+	reponse = app.response_class(response = 'b1-c3', status=200)
+	return reponse
 
+@app.route("/newgame")
+def newgame():
+  board.reset()
+  response = app.response_class(
+    response = board.fen(),
+    status=200
+  )
+  return response
 
 app.run(debug=True)
